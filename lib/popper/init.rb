3cf378244f6312263e562fad6e110159659d4613
pyama86 <pyama@pepabo.com>
2015-09-14 15:57:43 +0900

    update sample config

--- lib/popper/init.rb
+++ lib/popper/init.rb
@@ -12,7 +12,46 @@ module Popper
     end
 
     def self.sample_config
-      File.read('lib/popper/sample/config.txt')
+      <<-EOS
+[popper]
+slack_webhook_url = "https://test.slack.com"
+slack_user = "popper"
+git_token = "test"
+ghe_token = "test"
+ghe_url = "http://ghe.example.com"
+
+[example.login]
+server = "mail.examplejp"
+user = "examplle_user"
+password = "examplle_pass"
+
+[default.condition]
+subject = ["^(?!.*Re:).+$"]
+
+[default.action.slack]
+webhook_url = "webhook_url"
+user = "slack"
+channel = "#default_channel"
+message = "default message"
+
+[example.default.condition]
+subject = [".*default.*"]
+
+[example.rules.normal_log.condition]
+subject = ".*example.*"
+body = ".*example.*"
+
+[example.rules.normal_log.action.slack]
+channel = "#test"
+mentions = ["@test"]
+message = "test message"
+
+[example.rules.normal_log.action.git]
+repo = "test/example"
+
+[example.rules.normal_log.action.ghe]
+repo = "test/example"
+      EOS
     end
   end
 end
--- lib/popper/version.rb
+++ lib/popper/version.rb
@@ -1,3 +1,3 @@
 module Popper
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
