b20ccf4bef9828a61b50a96095464256521cde7c
pyama86 <pyama@pepabo.com>
2015-09-15 12:06:01 +0900

    update spec

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -9,14 +9,18 @@ describe Popper::Config do
   end
 
   context 'condition' do
-    it { expect(Popper.configure.accounts.first.condition_by_rule("normal_log").subject).to eq ["^(?!.*Re:).+$", ".*example.*"] }
-    it { expect(Popper.configure.accounts.first.condition_by_rule("normal_log").body).to eq [".*account default body.*", ".*example.*"] }
-    it { expect(Popper.configure.accounts.first.condition_by_rule("error_log").subject).to eq ["^(?!.*Re:).+$", ".*example2.*"] }
-    it { expect(Popper.configure.accounts.first.condition_by_rule("error_log").body).to eq [".*account default body.*", ".*example2.*"] }
+    it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").subject).to eq ["^(?!.*Re:).+$", ".*account_rule_subject.*"] }
+    it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").body).to eq [".*account_default_body.*"] }
+    it { expect(Popper.configure.accounts.last.condition_by_rule("test_rule").subject).to eq ["^(?!.*Re:).+$", ".*account_rule_subject_2.*"] }
+    it { expect(Popper.configure.accounts.last.condition_by_rule("test_rule").body).to eq [".*account_default_body_2.*"] }
   end
 
   context 'action' do
-    it { expect(Popper.configure.accounts.first.action_by_rule("error_log").git.token).to eq "account_default_token" }
-    it { expect(Popper.configure.accounts.first.action_by_rule("error_log").slack.channel).to eq "#example_2" }
+    it { expect(Popper.configure.accounts.first.action_by_rule("test_rule").ghe.token).to eq "test_token" }
+    it { expect(Popper.configure.accounts.first.action_by_rule("test_rule").ghe.url).to eq "https://ghe.example.com" }
+    it { expect(Popper.configure.accounts.first.action_by_rule("test_rule").ghe.repo).to eq "example/rule" }
+    it { expect(Popper.configure.accounts.last.action_by_rule("test_rule").ghe.token).to eq "test_token" }
+    it { expect(Popper.configure.accounts.last.action_by_rule("test_rule").ghe.url).to eq "https://2.ghe.example.com" }
+    it { expect(Popper.configure.accounts.last.action_by_rule("test_rule").ghe.repo).to eq "example/rule_2" }
   end
 end
--- spec/lib/pop_spec.rb
+++ spec/lib/pop_spec.rb
@@ -3,42 +3,46 @@ require 'slack-notifier'
 require 'octokit'
 describe Popper::Pop do
   before do
-    options = {}
-    options[:config] = 'spec/fixture/popper.conf'
     allow(File).to receive(:open).and_yield(Dummy.new)
     allow(File).to receive(:write).and_return(true)
-    Popper.load_config(options)
-    Popper.init_logger(options)
   end
 
   describe 'run' do
     before do
+      options = {}
+      options[:config] = 'spec/fixture/popper_run.conf'
+      Popper.load_config(options)
+      Popper.init_logger(options)
+
       allow_any_instance_of(Net::POP3).to receive(:start).and_yield(dummy_pop)
+
+      # slack
       expect_any_instance_of(Slack::Notifier).to receive(:ping).with(
-        "test message @test git:https://test.git.com/v3/issues/#123 ghe:https://test.ghe.com/v3/issues/#123",
+        "default_action_slack git:https://test.git.com/v3/issues/#123 ghe:https://test.ghe.com/v3/issues/#123",
         {
           attachments: [
             {
               pretext: "2015-09-10T16:20:10+09:00",
-              text: "test example subject",
+              text: "default_condition account_rule_condition_subject",
               color: "good"
             }
           ]
         }
       )
+
       # github
       allow_any_instance_of(Octokit::Client).to receive(:create_issue).with(
-        "test/example",
-        "test example subject",
-        "test example body account default body\n",
+        "example/account_rule_action_git",
+        "default_condition account_rule_condition_subject",
+        "account_default_condition account_rule_condition_body\n"
       ).and_return(
         { html_url: "https://test.git.com/v3/issues/#123" }
       )
       # ghe
       allow_any_instance_of(Octokit::Client).to receive(:create_issue).with(
-        "example/default",
-        "test example subject",
-        "test example body account default body\n",
+        "example/account_rule_action_ghe",
+        "default_condition account_rule_condition_subject",
+        "account_default_condition account_rule_condition_body\n"
       ).and_return(
         { html_url: "https://test.ghe.com/v3/issues/#123" }
       )
@@ -48,10 +52,16 @@ describe Popper::Pop do
   end
 
   describe 'matching?' do
+    before do
+      options = {}
+      options[:config] = 'spec/fixture/popper_matching.conf'
+      Popper.load_config(options)
+      Popper.init_logger(options)
+    end
+
     it { expect(described_class.matching?(Popper.configure.accounts.first, ok_mail)).to be_truthy }
+    it { expect(described_class.matching?(Popper.configure.accounts.first, ng_subject_mail)).to be_falsey }
     it { expect(described_class.matching?(Popper.configure.accounts.first, ng_body_mail)).to be_falsey }
-    it { expect(described_class.matching?(Popper.configure.accounts.first, ng_mail)).to be_falsey }
-    it { expect(described_class.matching?(Popper.configure.accounts.first, reply_mail)).to be_falsey }
   end
 end
 
@@ -69,7 +79,7 @@ def dummy_pop
 
     def pop_mail.mail
       <<-EOS
-Delivered-To: example@example.com\r\nReceived: (qmail 5075 invoked from network); 10 Sep 2015 16:20:10 +0900\r\nTo: example@example.com\r\nSubject: test example subject\r\nFrom:no-reply@example.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=ISO-2022-JP\r\nContent-Transfer-Encoding: 7bit\r\nMessage-Id: <20150910072010.0545296845A@example.com>\r\nDate: Thu, 10 Sep 2015 16:20:10 +0900 (JST)\r\n\r\ntest example body account default body
+Delivered-To: example@example.com\r\nReceived: (qmail 5075 invoked from network); 10 Sep 2015 16:20:10 +0900\r\nTo: example@example.com\r\nSubject: default_condition account_rule_condition_subject\r\nFrom:no-reply@example.com\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=ISO-2022-JP\r\nContent-Transfer-Encoding: 7bit\r\nMessage-Id: <20150910072010.0545296845A@example.com>\r\nDate: Thu, 10 Sep 2015 16:20:10 +0900 (JST)\r\n\r\naccount_default_condition account_rule_condition_body
       EOS
     end
     [
@@ -81,28 +91,22 @@ end
 
 def ok_mail
   mail = EncodeMail.new
-  mail.subject = "test example subject"
-  mail.body    = "test example word account default body"
+  mail.subject = "test ok"
+  mail.body    = "test ok"
   mail
 end
 
 def ng_body_mail
   mail = EncodeMail.new
-  mail.subject = "test example subject"
-  mail.body    = "test nomatch word"
+  mail.subject = "test ok"
+  mail.body    = "test ng"
   mail
 end
 
-def ng_mail
+def ng_subject_mail
   mail = EncodeMail.new
-  mail.subject = "test nomatch subject"
-  mail.body    = "test nomatch word"
+  mail.subject = "test ng"
+  mail.body    = "test ok"
   mail
 end
 
-def reply_mail
-  mail = EncodeMail.new
-  mail.subject = "Re: test example subject"
-  mail.body    = "test example word"
-  mail
-end
