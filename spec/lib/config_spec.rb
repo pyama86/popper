b2ac73b5403e94cfe87002a15de45372afcc6a0b
pyama86 <pyama@pepabo.com>
2015-10-19 12:54:22 +0900

    add global config test

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -8,6 +8,11 @@ describe Popper::Config do
     Popper.load_config(options)
   end
 
+  context 'global' do
+    it { expect(Popper.configure.global.interval).to eq 60 }
+    it { expect(Popper.configure.global.work_dir).to eq "/var/tmp" }
+  end
+
   context 'condition' do
     it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").subject).to eq ["^(?!.*Re:).+$", ".*account_rule_subject.*"] }
     it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").body).to eq [".*account_default_body.*"] }
