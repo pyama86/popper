5faa9ce88dfc84b986f3ef71be6fb283d813526c
pyama86 <pyama@pepabo.com>
2015-11-10 12:31:55 +0900

    fix spec

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -14,9 +14,9 @@ describe Popper::Config do
 
   context 'condition' do
     it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").subject).to eq ["^(?!.*Re:).+$", ".*account_rule_subject.*"] }
-    it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").body).to eq [".*account_default_body.*"] }
+    it { expect(Popper.configure.accounts.first.condition_by_rule("test_rule").utf_body).to eq [".*account_default_body.*"] }
     it { expect(Popper.configure.accounts.last.condition_by_rule("test_rule").subject).to eq ["^(?!.*Re:).+$", ".*account_rule_subject_2.*"] }
-    it { expect(Popper.configure.accounts.last.condition_by_rule("test_rule").body).to eq [".*account_default_body_2.*"] }
+    it { expect(Popper.configure.accounts.last.condition_by_rule("test_rule").utf_body).to eq [".*account_default_body_2.*"] }
   end
 
   context 'action' do
